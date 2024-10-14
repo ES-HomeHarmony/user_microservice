7e8a873c4ec04adf631fecb5263c88cbb8bf068f
Roberto Castro <robertorcastro@ua.pt>
2024-10-14 23:43:31 +0100

    SCRUM-13 Users are saved to mySQL DB

---
+++ User_MicroService/app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    cognito_id = Column(String(255), index=True)
+    name = Column(String(100))
+    email = Column(String(100), unique=True, index=True)
+
