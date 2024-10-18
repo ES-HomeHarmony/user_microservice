721394b9a12f589c9d6676484953854d9c9a645a
Roberto Castro <robertorcastro@ua.pt>
2024-10-18 21:29:12 +0100

    SCRUM-13 Try Github workflow 8

--- User_MicroService/app/database.py
+++ User_MicroService/app/database.py
@@ -2,12 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# if os.getenv("TESTING") == '1':
-#     URL_DATABASE = 'mysql+pymysql://test_user:test_password@localhost:3307/test_user_db'
-# else:
-#     # Use the actual MySQL database for normal operations
-#     URL_DATABASE = 'mysql+pymysql://user:VERY_SECURE_PASSWORD@localhost:3306/user_db'
-
 URL_DATABASE = 'mysql+pymysql://user:VERY_SECURE_PASSWORD@localhost:3306/user_db'
 
 engine = create_engine(URL_DATABASE)
